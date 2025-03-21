6e6430df210169a4040dcd864f0346791d3adcaa
yijing <po910216@gmail.com>
2025-03-21 16:06:51 +0800

    綠界金流串接

--- config/server.config.js
+++ config/server.config.js
@@ -1,5 +1,6 @@
 import 'dotenv/config.js'
 
+
 const env = process.env.NODE_ENV || 'development'
 
 export const serverConfig = {
@@ -14,12 +15,13 @@ export const serverConfig = {
   // 後端伺服器佈置後的網域名稱，與cookie有關
   domain: env === 'development' ? '' : 'xxxxxx.vercel.app',
   // ethereal
-  smtp: {
-    provider: 'ethereal',
-    host: 'smtp.ethereal.email',
-    user: 'mittie.daniel91@ethereal.email',
-    pass: 'b6en9s7EqjP9EPVKkd',
-  },
+  // smtp: {
+  //   provider: 'ethereal',
+  //   host: 'smtp.ethereal.email',
+  //   user: 'mittie.daniel91@ethereal.email',
+  //   pass: 'b6en9s7EqjP9EPVKkd',
+  // },
+
   // gmail
   // smtp: {
   //   provider: 'gmail',
@@ -27,26 +29,30 @@ export const serverConfig = {
   //   user: 'xxxxxx@gmail.com',
   //   pass: 'xxxxxxxx',
   // },
-  jwt: {
-    secret: 'access_token_secret',
-  },
-  otp: {
-    secret: 'otp_secret',
-    expire: 5 * 60 * 1000, // 5 分鐘
-  },
+
+  // jwt: {
+  //   secret: 'access_token_secret',
+  // },
+
+  // otp: {
+  //   secret: 'otp_secret',
+  //   expire: 5 * 60 * 1000, // 5 分鐘
+  // },
+
   // local development
-  lineLogin: {
-    development: {
-      channelId: '123456789',
-      channelSecret: 'xxxxxxxxxxxx',
-      callbackUrl: 'http://localhost:3000/user/line-login',
-    },
-    production: {
-      channelId: '',
-      channelSecret: '',
-      callbackUrl: 'https://xxxxx.vercel.app/user/line-login',
-    },
-  },
+  // lineLogin: {
+  //   development: {
+  //     channelId: '123456789',
+  //     channelSecret: 'xxxxxxxxxxxx',
+  //     callbackUrl: 'http://localhost:3000/user/line-login',
+  //   },
+  //   production: {
+  //     channelId: '',
+  //     channelSecret: '',
+  //     callbackUrl: 'https://xxxxx.vercel.app/user/line-login',
+  //   },
+  // },
+
   // 前端接回導向的網址
   ship711: {
     development: {
@@ -56,20 +62,21 @@ export const serverConfig = {
       callbackUrl: 'https://xxxxx.vercel.app/ship/callback',
     },
   },
-  linePay: {
-    development: {
-      channelId: '123456789',
-      channelSecret: 'xxxxxxxxxxxx',
-      confirmUrl: 'http://localhost:3000/line-pay',
-      cancelUrl: 'http://localhost:3000/line-pay/cancel',
-    },
-    production: {
-      channelId: '',
-      channelSecret: '',
-      confirmUrl: 'https://next-app-raw.vercel.app/line-pay',
-      cancelUrl: 'https://next-app-raw.vercel.app/line-pay/cancel',
-    },
-  },
+  // linePay: {
+  //   development: {
+  //     channelId: '123456789',
+  //     channelSecret: 'xxxxxxxxxxxx',
+  //     confirmUrl: 'http://localhost:3000/line-pay',
+  //     cancelUrl: 'http://localhost:3000/line-pay/cancel',
+  //   },
+  //   production: {
+  //     channelId: '',
+  //     channelSecret: '',
+  //     confirmUrl: 'https://next-app-raw.vercel.app/line-pay',
+  //     cancelUrl: 'https://next-app-raw.vercel.app/line-pay/cancel',
+  //   },
+  // },
+
   // 目前沒有使用這個設定，直接寫在 routes/ecpay-test-only.js 中
   // ecpay: {
   //   test: true,
